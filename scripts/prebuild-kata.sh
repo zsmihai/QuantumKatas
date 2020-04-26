1c3f8196cfa57cc00e01e8148f7f3468081f98a5
Andres Paz <anpaz@microsoft.com>
2020-04-26 13:06:00 -0700

    pushd

--- scripts/prebuild-kata.sh
+++ scripts/prebuild-kata.sh
@@ -7,8 +7,12 @@ echo "Prebuilding: $KATA_NOTEBOOK in $KATA_FOLDER kata..."
 #dotnet build $KATA_FOLDER
 
 start=`date +%s`
+
 # All we need to do is import the qsharp package, this will take care of building the Workspace
+pushd $KATA_FOLDER
 python -c "import qsharp"
+popd
+
 end=`date +%s`
 
 echo total time `expr $end - $start`
